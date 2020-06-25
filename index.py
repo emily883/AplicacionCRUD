9aac3845a1798ece0fa5e2a1f788e58aa4b6bee1
Emily <emily.moraima.cruz@hotmail.com>
2020-06-24 22:50:39 -0400

    agregar personas a base de datos y acceder a la base tal cual

--- index.py
+++ index.py
@@ -31,24 +31,102 @@ def conectar():
     micursor=miConexion.cursor()
     messagebox.showinfo("Conexion", "Base de datos creada y conectada correctamente")
 
-
-def crear():
-    global micursor
-    global miConexion
-    valor=messagebox.askquestion("Crear", "Deseas crear la base de datos?")
+    valor=messagebox.askquestion("Crear", "Deseas crear la tabla?")
   
 
     if valor=="yes":
 
-        micursor.execute("CREATE TABLE PERSONAS (ID INTEGER,NOMBRE VARCHAR(50),PASSWORD VARCHAR(20),APELLIDO VARHCAR(20),DIRECCION VARCHAR(50), COMENTARIOS VARCHAR(100))")
-        miConexion.commit()
-        miConexion.close()
-        messagebox.showinfo("Crear", "Tabla en base de datos creada exitosamente")
+        try:
+      
+            micursor.execute('''
+                    CREATE TABLE PERSONAS (
+                    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+                    NOMBRE VARCHAR(50),
+                    PASSWORD VARCHAR(20),
+                    APELLIDO VARCHAR(20),
+                    DIRECCION VARCHAR(50),
+                    COMENTARIOS VARCHAR(100))
+                    ''')
+         
+            messagebox.showinfo("Crear", "Tabla en base de datos creada exitosamente")
+            miConexion.commit()
+            miConexion.close()
+            
+
+        except: 
+            messagebox.showwarning("ATENCION", "LA BASE DE DATOS YA EXISTE")
+
+
+
+
+
+def limpiarCampos():
+    miID.set("")   
+    Miname.set("") 
+    passw.set("") 
+    ape.set("") 
+    dire.set("") 
+    comment.set("")
+    
+
+def crear():
+    global miConexion
+    global micursor
+
+    miConexion=sqlite3.connect("ListaDePersonas")
+    micursor=miConexion.cursor()
+    instrucciones="INSERT INTO PERSONAS VALUES('{0}', '{1}','{2}', '{3}','{4}', '{5}')".format(miID.get(), Miname.get(), passw.get(), ape.get(), dire.get(), comment.get())
+
+    micursor.execute(instrucciones)
+
+    miConexion.commit()
+
+
+    miConexion.close()
+
+    limpiarCampos()
+
+
+def leer():
+    miConexion=sqlite3.connect("ListaDePersonas")
+    micursor=miConexion.cursor()
+
+    micursor.execute("SELECT* FROM PERSONAS")
+    personitas=micursor.fetchall()
+
+    for dato in personitas:
+            miID.set(dato[0])   
+            Miname.set(dato[1]) 
+            passw.set(dato[2]) 
+            ape.set(dato[3]) 
+            dire.set(dato[4]) 
+            comment.set(dato[5])
+        
+
+
+
+
+    
+
+
+
+
+
+   
+    miConexion.commit()
+
+
+    miConexion.close()
+
+
+
+
+
 
 
 #______________________MENUS______________________________
 borrar=Menu(barramenu, tearoff=0)
-borrar.add_command(label="Borrar campos")
+borrar.add_command(label="Borrar campos", command=limpiarCampos)
 
 ayuda=Menu(barramenu, tearoff=0)
 ayuda.add_command(label="Licencia")
@@ -56,7 +134,7 @@ ayuda.add_command(label="Acerca de...")
 
 CRUD=Menu(barramenu, tearoff=0 )
 CRUD.add_command(label="Crear", command=crear)
-CRUD.add_command(label="Leer")
+CRUD.add_command(label="Leer", command=leer)
 CRUD.add_command(label="Actualizar")
 CRUD.add_command(label="Borrar")
 
@@ -81,19 +159,29 @@ Label(miframe, text="Direccion").grid(row=4, column=1, padx=5, pady=4)
 Label(miframe, text="Comentario").grid(row=5, column=1, padx=5, pady=4)
 
 #----------------------------------------------------------------
-aidi = Entry(miframe).grid(row=0, column=2, padx=5, pady=5)
-nombre =Entry(miframe, justify = "center").grid(row=1, column=2, padx=5, pady=5)
-contra = Entry(miframe, show="*", justify="center").grid(row=2, column=2, padx=5, pady=5)
-apellido = Entry(miframe, justify="center").grid(row=3, column=2, padx=5, pady=5)
-direccion = Entry(miframe).grid(row=4, column=2, padx=5, pady=5)
-comentario = Text(miframe, width=16, height=5).grid(row=5, column=2, padx=5, pady=5)
+miID=StringVar()
+Miname=StringVar()
+passw=StringVar()
+ape=StringVar()
+dire=StringVar()
+comment=StringVar()
+
+
+
+aidi = Entry(miframe, textvariable=miID).grid(row=0, column=2, padx=5, pady=5)
+nombre =Entry(miframe, justify = "center", textvariable=Miname).grid(row=1, column=2, padx=5, pady=5)
+contra = Entry(miframe, show="*", justify="center", textvariable=passw).grid(row=2, column=2, padx=5, pady=5)
+apellido = Entry(miframe, justify="center", textvariable=ape).grid(row=3, column=2, padx=5, pady=5)
+direccion = Entry(miframe, textvariable=dire).grid(row=4, column=2, padx=5, pady=5)
+comentario = Entry(miframe, textvariable=comment).grid(row=5, column=2, padx=5, pady=5)
+
 '''
 scrollvert=Scrollbar(miframe, command=comentario.yview).grid(row=5, column=3, sticky="nsew")
 comentario.config(yscrollcommand=scrollvert.set)'''
 #-----------------------botones 2 frame--------------------------------
 
 crear=Button(frame, text="Crear", command=crear).grid(row=1, column=0, padx=7, pady=7)
-leer=Button(frame, text="Leer").grid(row=1, column=1, padx=7, pady=7)
+leer=Button(frame, text="Leer", command=leer).grid(row=1, column=1, padx=7, pady=7)
 actualizar=Button(frame, text="Actualizar").grid(row=1, column=2, padx=7, pady=7)
 eliminar=Button(frame, text="Eliminar").grid(row=1, column=3, padx=7, pady=7)
 
